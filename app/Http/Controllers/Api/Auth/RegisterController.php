374b84251b1efec7d9d6faa859ca8a39f81e0db7
ftp <sidorosp_dr@fayden11.beget.ru>
2021-10-11 17:53:09 +0300

    [dev1] create swagger summaty for registration metod

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -7,25 +7,74 @@ use App\Http\Controllers\Controller;
 use App\Servises\AuthService;
 use App\Http\Requests\Api\Auth\RegisterFormRequest;
 
+/**
+ * Registration user class
+ */
 class RegisterController extends Controller
 {
     /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @OA\Post(
+     *     path="/register",
+     *     summary="Регистрация пользователя",
+     *     tags={"auth"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     format="email"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                      type="string",
+     *                       writeOnly="true"
+     *                 ),
+     *                
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             oneOf={
+     *                 @OA\Schema(ref="#/components/schemas/Result"),
+     *                 @OA\Schema(type="boolean")
+     *             },
+     *             @OA\Examples(example="result", value={"success": true}, summary="An result object."),
+     *             @OA\Examples(example="bool", value=false, summary="A boolean value."),
+     *         )
+     *     )
+     * )
+     */
+
+    /**
+     * @OA\Schema(
+     *  schema="Result",
+     *  title="Sample schema for using references",
+     * 	@OA\Property(
+     * 		property="status",
+     * 		type="string"
+     * 	),
+     * 	@OA\Property(
+     * 		property="error",
+     * 		type="string"
+     * 	)
+     * )
      */
     public function register(RegisterFormRequest $request)
     {
         try {
             $result =   AuthService::register($request);
-           // $message = 'You were successfully registered. Use your email and password to sign in.';
             return HelperJson::sendAnswer($result, config('message.success'), 200);
-
         } catch (\Throwable $th) {
-            return response()->json([
-                'message' => 'error'
-            ], 400);
+            return abort(400, $th->getMessage());
         }
     }
 }
